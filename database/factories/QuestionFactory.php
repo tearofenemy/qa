857cd33d109823778cc0289da3baba477e2d0cab
tearofenemy <calm.snow98@gmail.com>
2020-01-30 22:36:51 +0300

    add some data to database

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5, 8)), "."),
+        'slug' => str_slug($faker->sentence(rand(1, 2))),
+        'body' => $faker->paragraphs(rand(1, 3), true),
+        'views' => rand(10, 15),
+        'answers' => rand(10, 15),
+        'votes' => rand(-5, 10)
+    ];
+});
