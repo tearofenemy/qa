66a6f23aebd05eef82f24e140208a4b27950c925
tearofenemy <calm.snow98@gmail.com>
2020-02-02 17:18:26 +0300

    display user who asked the question, and date

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,13 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
+                <div class="card-header">
+                    All Questions
+                </div>
 
                 <div class="card-body">
                     @foreach ($questions as $question)
                         <div class="media">
                             <div class="media-body">
-                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->date }}</small>
+                                </p>
                                 {{ str_limit($question->body, 250) }}
                             </div>                        
                         </div>
