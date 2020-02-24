b29cf395ddea5bee4174aec582bb13e8e38f15ee
tearofenemy <calm.snow98@gmail.com>
2020-02-24 16:36:39 +0300

    fix update method

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -15,7 +15,7 @@
                 </div>
 
                 <div class="card-body">
-                    <form action="{{ route('questions.update', $question->id) }}" method="post">
+                    <form action="{{ route('questions.update', $question->slug) }}" method="post">
                         @method('PUT')
                         @include('questions._form', ['btnText' => 'Update Quesiton'])
                     </form>
